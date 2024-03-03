3d2e06c1288301b2ff95eb19f3ce21e7c529d64b
alaeddine2003 <161325066+alaeddine2003@users.noreply.github.com>
2024-03-03 11:23:41 +0000

    nadjib

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import './login.css';
+
 
 const Login = () => {
   return (
-    <div>
-      login
+    <div className='' id='login'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-5'>
+          <div className='title text-light'></div>
+          </div>
+          <div className='col-md-7'>
+          <div className='title'></div>
+          </div>
+        </div>
+      </div> 
     </div>
   )
 }
