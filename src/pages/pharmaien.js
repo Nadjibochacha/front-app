74a7ab3849469161d5c7dded541149f19d6d76f5
CHACHA MOHAMMED NADJIB <139798416+Nadjibochacha@users.noreply.github.com>
2024-03-23 18:52:07 +0100

    pharmacien page

--- src/pages/pharmaien.js
+++ src/pages/pharmaien.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './pharmacy.css';
+import shopp from '../images/logo.png';
+import { FcStatistics } from "react-icons/fc";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { FaUserCircle } from "react-icons/fa";
 
 const Pharmaien = () => {
   return (
-    <div>
-      pharmacien dashboard
+    <div id='pharmacy'>
+        <div className='menu'>
+          <div className='name'>
+            <img src={shopp} alt=''/>
+            <h3>SalCo pharmacy</h3> 
+          </div>
+          <ul className=''>
+            <li>
+              <a href=''><FcStatistics /><span> statistics</span></a> 
+            </li>
+            <li><a href=''><MdProductionQuantityLimits/><span>products</span></a></li>
+            <li><a href=''><FaUserCircle/><span>users</span></a></li>
+            <li>
+              <a href=''><FcStatistics /><span> statistics</span></a> 
+            </li>
+            <li><a href=''><MdProductionQuantityLimits/><span>products</span></a></li>
+            <li><a href=''><FaUserCircle/><span>users</span></a></li>
+          </ul>
+        </div>
+        <div className='content w-100'>
+          <div className='globl-title'>
+            <h3 >pharmacy dashboard</h3>
+            <p>A quick data overview of the inventory.</p>
+          </div>
+        </div>
     </div>
   )
 }
